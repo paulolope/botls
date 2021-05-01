58a5f03a7f3b7bcb7b5825c0a2b2686aa6be71a9
paulolope <paulolope91@gmail.com>
2021-04-30 23:39:08 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,68 @@
+const help = (prefix) => {
+	return `
+「 *Big Braim* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : Brayan
+  ❏ Como instalar o BOT: https://youtu.be/M4fPhiIqhzA
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  └─ ❏ ${prefix}quotemaker
+◪ *PERGUNTAS*
+  │
+  ├─ ❏ ${prefix}Whatis
+  ├─ ❏ ${prefix}QuandoÉo
+  ├─ ❏ ${prefix}Avaliar
+  └─ ❏ ${prefix}VocêPode
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play (só funciona de dia)
+  └─ ❏ ${prefix}tts
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ *ANIMES*
+  │
+  ├─ ❏ ${prefix}pokemon (às vezes nn funfa)
+  └─ ❏ ${prefix}nekonime
+◪ *INFORMAÇÃO*
+  │
+  ├─ ❏ ${prefix}bahasa (idiomas)
+  └─ ❏ ${prefix}kodenegara (DDI)
+◪ *DONO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}wame (DDD+número)
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}exe
+  └─ ❏ ${prefix}qrcode (nome)
+`
+}
+
+exports.help = help
